d117a4ee0202142a92e013adb8bb63f7750a3aeb
2003-ABi <poudelabhishek439@gmail.com>
2026-06-24 09:45:32 +0100

    Snaplink

---
+++ src/main/java/com/example/Snaplink/SnaplinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.Snaplink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnaplinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnaplinkApplication.class, args);
+	}
+
+}
